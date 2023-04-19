362690280855ce25b883843ea23f01e53971ffeb
Old Bread <noofnafnif@gmail.com>
2023-04-19 21:35:31 +0300

    constructor

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -1,22 +1,36 @@
 package ru.netology.radio;
 
 public class Radio {
+    private int maxRadioStations = 9;
     private int radioStations;
     private int volume;
 
+    public Radio(int maxRadioStations){
+        this.maxRadioStations = maxRadioStations;
+
+    }
+    public Radio (){
+        maxRadioStations = 9;
+    }
+
     public int getRadioStations() {
         return radioStations;
+    }
+
+    public int getMaxRadioStations(){
+        return maxRadioStations;
     }
 
     public int getVolume() {
         return volume;
     }
 
+
     public void setRadioStations(int newRadioStations) {
         if (newRadioStations <= 0) {
             return;
         }
-        if (newRadioStations > 9) {
+        if (newRadioStations > maxRadioStations) {
             return;
         }
         radioStations = newRadioStations;
